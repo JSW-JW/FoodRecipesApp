82fdfd978c3bc796f0e431ab6ac3497987a63f7e
Seungwon <jshtmddnjs@naver.com>
2020-10-18 13:29:14 +0900

    Retrofit service singleton & object model for converting repsponse into

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/RecipeApi.java
@@ -0,0 +1,5 @@
+package com.codingwithmitch.foodrecipes.requests;
+
+public interface RecipeApi {
+
+}
---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.codingwithmitch.foodrecipes.utils;
+
+public class Constants {
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+
+    public static final String API_KEY = "";
+
+
+}
