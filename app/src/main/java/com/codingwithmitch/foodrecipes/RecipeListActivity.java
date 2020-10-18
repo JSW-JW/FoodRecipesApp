181800a07be205090518f3a1c194989a22220f94
Seungwon <jshtmddnjs@naver.com>
2020-10-18 15:03:11 +0900

    testRetrofitRequest & compile option java 1_8 debugging solved

--- app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/RecipeListActivity.java
@@ -1,9 +1,27 @@
 package com.codingwithmitch.foodrecipes;
 
+import android.app.Service;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.codingwithmitch.foodrecipes.models.Recipe;
+import com.codingwithmitch.foodrecipes.requests.RecipeApi;
+import com.codingwithmitch.foodrecipes.requests.ServiceGenerator;
+import com.codingwithmitch.foodrecipes.requests.responses.RecipeSearchResponse;
+import com.codingwithmitch.foodrecipes.utils.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class RecipeListActivity extends BaseActivity {
+
+    private static final String TAG = "RecipeListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,13 +31,45 @@ public class RecipeListActivity extends BaseActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mProgressBar.getVisibility() == View.VISIBLE) {
-                    showProgressBar(false);
-                }
-                else {
-                    showProgressBar(true);
-                }
+                testRetrofitRequest();
             }
         });
+    }
+
+    private void testRetrofitRequest(){
+        RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
+
+        Call<RecipeSearchResponse> responseCall = recipeApi
+                .searchRecipe(
+                        Constants.API_KEY,
+                        "chicken breast",
+                        "1"
+                );
+        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+            @Override
+            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+                Log.d(TAG, "onResponse: server response: " + response.toString());
+                if(response.code() == 200){
+                    Log.d(TAG, "onResponse: " + response.body().toString());
+                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+                    for(Recipe recipe : recipes) {
+                        Log.d(TAG, "onResponse: recipe: " + recipe.getTitle());
+                    }
+                }
+                else {
+                    try {
+                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+
+            }
+        });
+
     }
 }
