3a35f35979b89c3b49262797f3f5f254095299f2
Seungwon <jshtmddnjs@naver.com>
2020-10-20 11:55:51 +0900

    glide & casting viewholder to RecipeViewHolder

--- app/src/main/java/com/codingwithmitch/foodrecipes/adapters/RecipeRecyclerAdapter.java
+++ app/src/main/java/com/codingwithmitch/foodrecipes/adapters/RecipeRecyclerAdapter.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.codingwithmitch.foodrecipes.R;
 import com.codingwithmitch.foodrecipes.models.Recipe;
 
@@ -14,22 +16,31 @@ import java.util.List;
 
 public class RecipeRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<Recipe> mRecipes;
-    private OnRecipeListener mOnRecipeListner;
+    private OnRecipeListener mOnRecipeListener;
 
-    public RecipeRecyclerAdapter(List<Recipe> mRecipes, OnRecipeListener mOnRecipeListner) {
-        this.mRecipes = mRecipes;
-        this.mOnRecipeListner = mOnRecipeListner;
+    public RecipeRecyclerAdapter(List<Recipe> recipes, OnRecipeListener onRecipeListener) {
+        this.mRecipes = recipes;
+        this.mOnRecipeListener = onRecipeListener;
     }
 
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_recipe_list_item, parent, false);
-        return new RecipeViewHolder(view, mOnRecipeListner);
+        return new RecipeViewHolder(view, mOnRecipeListener);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        RequestOptions requestOptions = new RequestOptions()
+                .placeholder(R.drawable.ic_launcher_background);
+
+        Glide.with(holder.itemView.getContext())
+                .setDefaultRequestOptions(requestOptions)
+                .load(mRecipes.get(position).getImage_url())
+                .into(((RecipeViewHolder)holder).image);
+
         ((RecipeViewHolder)holder).title.setText(mRecipes.get(position).getTitle());
         ((RecipeViewHolder)holder).publisher.setText(mRecipes.get(position).getPublisher());
         ((RecipeViewHolder)holder).socialScore.setText(String.valueOf(Math.round(mRecipes.get(position).getSocial_rank())));
